e1e0922312c245734eb499cd6eac34b76210690b
Maksim Gavrilov <maksim_gavrilov_2014@bk.ru>
2023-01-06 13:50:47 +0400

    init commit

---
+++ src/main/java/com/gavrilov/plants/AdvancedghApplication.java
@@ -0,0 +1,13 @@
+package com.gavrilov.plants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedghApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedghApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gavrilov/plants/AdvancedghApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gavrilov.plants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedghApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
